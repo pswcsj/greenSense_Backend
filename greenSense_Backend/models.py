d85c4c2853fe96c0ae64e36ee44b2e11f5b1d763
a <pswxsj@naver.com>
2022-08-30 20:50:24 +0900

    Fix: naming 규칙 반영

--- greenSense_Backend/models.py
+++ greenSense_Backend/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser)
+
 # 채팅, 질문 대답
 
 USER_TYPE = ['Under', 'Master', 'Doctor']
+
+
 class User(AbstractBaseUser):
     name = models.CharField()
     nickname = models.CharField()
@@ -20,16 +23,19 @@ class Chat(models.Model):
     lastMessage = models.TextField()
     createdAt = models.DateTimeField()
 
+
 ATTACHMENT_TYPE = ['Image', 'Video', 'None']
+
+
 class Message(models.Model):
     chat = models.ForeignKey(Chat)
-    fromUser = models.ForeignKey(User)
-    toUser = models.ForeignKey(User)
+    from_user = models.ForeignKey(User)
+    to_user = models.ForeignKey(User)
     text = models.TextField()
     attachment = models.FileField(upload_to='message')
-    attachmentType = models.CharField(choices=ATTACHMENT_TYPE, default='None')
-    createdAt =models.DateTimeField()
-    isRead = models.BooleanField(default=False)
+    attachment_type = models.CharField(choices=ATTACHMENT_TYPE, default='None')
+    created_at = models.DateTimeField()
+    is_read = models.BooleanField(default=False)
 
 
 class Question(models.Model):
@@ -49,12 +55,6 @@ class Question(models.Model):
     attachment = models.FileField(upload_to='question')
     text = models.TextField()
     chat = models.ForeignKey(Chat)
-    isEnd = models.BooleanField(default = False)
-    createdAt = models.DateTimeField()
-    endAt = models.DateTimeField()
-
-
-
-
-
-
+    is_end = models.BooleanField(default=False)
+    created_at = models.DateTimeField()
+    end_at = models.DateTimeField()
