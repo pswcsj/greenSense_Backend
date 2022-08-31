6ec969c2b06436192024369053d25cf149c0d05f
luftroffe <107554680+luftroffe@users.noreply.github.com>
2022-08-31 15:06:55 +0900

    feat: Add views, serializers, urls

--- greenSense_Backend/urls.py
+++ greenSense_Backend/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from greenSense_Backend import views
+
+router = DefaultRouter()
+router.register('user', views.UserViewSet)
+router.register('chat', views.ChatViewSet)
+router.register('message', views.MessageViewSet)
+router.register('question', views.QuestionViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
--- greenSense_Backend/views.py
+++ greenSense_Backend/views.py
@@ -1,33 +1,24 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import viewsets
 
-from greenSense_Backend.models import Article
-from greenSense_Backend.serializers import ArticleSerializer
+from greenSense_Backend.models import *
+from greenSense_Backend.serializers import *
 
 
-# api_view 데코레이터 인자로 아무것도 입력하지 않으면 기본값 'GET'
-@api_view(["GET", "PUT", "DELETE"])
-def article_detail_api_view(request, pk):
-    try:
-        article = Article.objects.get(pk=pk)
-    except Article.DoesNotExist:
-        return Response({"error": {
-            "code": 404,
-            "message": "Article not found"
-        }}, status=status.HTTP_404_NOT_FOUND)
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
-    if request.method == 'GET':
-        serializer = ArticleSerializer(article)
-        return Response(serializer.data)
 
-    elif request.method == 'PUT':
-        serializer = ArticleSerializer(article, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ChatViewSet(viewsets.ModelViewSet):
+    queryset = Chat.objects.all()
+    serializer_class = ChatSerializer
 
-    elif request.method == 'DELETE':
-        article.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
